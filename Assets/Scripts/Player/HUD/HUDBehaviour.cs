39470b7183916ea4e62cfc43b9a2aa18eb8eb5cd
Morrison Cole <admin@morrisoncole.co.uk>
2023-01-13 20:29:31 +0900

    feat: play around with architecture for UI

---
+++ Assets/Scripts/Player/HUD/HUDBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+namespace Player.HUD
+{
+    public class HUDBehaviour : MonoBehaviour
+    {
+        private HealthView healthView;
+        private Health health;
+
+        private void OnEnable()
+        {
+            var menu = GetComponentInChildren<UIDocument>();
+            var root = menu.rootVisualElement;
+            health = new Health();
+            healthView = new HealthView(root, health);
+
+            StartCoroutine(RandomHealth());
+        }
+
+        private IEnumerator RandomHealth()
+        {
+            while (true)
+            {
+                health.Current = (int)(Random.value * health.Max);
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/HUD/HealthView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UIElements;
+
+namespace Player.HUD
+{
+    public class HealthView
+    {
+        private ProgressBar progressBar;
+
+        public HealthView(VisualElement root, Health health)
+        {
+            progressBar = root.Q<ProgressBar>();
+
+            health.HealthUpdated += OnHealthUpdated;
+        }
+
+        private void OnHealthUpdated(Health health)
+        {
+            progressBar.value = health.Current / (float)health.Max * 100f;
+        }
+    }
+}
