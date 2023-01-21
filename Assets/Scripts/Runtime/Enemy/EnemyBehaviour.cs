c9c5cc55c5b1e921ca48986e0a78a0235dcd902d
Morrison Cole <admin@morrisoncole.co.uk>
2023-01-21 21:56:14 +0900

    feat: add tracking enemies

--- Assets/Scripts/Runtime/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Runtime/Enemy/EnemyBehaviour.cs
@@ -6,6 +6,7 @@ namespace Com.ThirdNerve.Backfire.Runtime.Enemy
     public class EnemyBehaviour : MonoBehaviour
     {
         [SerializeField] private float desiredDistanceFromTarget = 5f;
+        [SerializeField] private float distanceThreshold = 0.6f;
         [SerializeField] private float maxSpeed = 50f;
         [SerializeField] private float acceleration = 1f;
         
@@ -16,32 +17,27 @@ namespace Com.ThirdNerve.Backfire.Runtime.Enemy
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _targetBehaviour = GetComponent<TargetBehaviour>();
-
-            StartCoroutine(UpdatePositionCoroutine());
-        }
-
-        private IEnumerator UpdatePositionCoroutine()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(3f);
-                
-                var targetPosition = _targetBehaviour.Target.position;
-
-                var distanceToTarget = Vector2.Distance(_rigidbody2D.position, targetPosition);
-                var vectorToTarget = targetPosition - _rigidbody2D.position;
-
-                var adjustedTargetPosition = vectorToTarget.normalized * desiredDistanceFromTarget;
-
-                _rigidbody2D.position = adjustedTargetPosition;
-            }
         }
 
         private void FixedUpdate()
         {
+            var targetPosition = _targetBehaviour.Target.position;
 
+            var distanceToTarget = Vector2.Distance(_rigidbody2D.position, targetPosition);
+            var vectorToTarget = targetPosition - _rigidbody2D.position;
+
+            var adjustedTargetPosition = _rigidbody2D.position + vectorToTarget.normalized * (distanceToTarget - desiredDistanceFromTarget);
+
+            var vectorToAdjustedTarget = adjustedTargetPosition - _rigidbody2D.position;
+
+            if (!(Mathf.Abs(distanceToTarget - desiredDistanceFromTarget) >= distanceThreshold))
+            {
+                _rigidbody2D.velocity = Vector2.zero;
+                return;
+            }
+            
             var newSpeed = _rigidbody2D.velocity.magnitude + acceleration * Time.deltaTime;
-            // _rigidbody2D.velocity = vectorToTarget.normalized * Mathf.Min(newSpeed, maxSpeed);
+            _rigidbody2D.velocity = vectorToAdjustedTarget.normalized * Mathf.Min(newSpeed, maxSpeed);
         }
     }
 }
