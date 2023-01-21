990125274ff749ad98f077ed0713df7e3c9b777b
TrisEdwards <trisj.edwards@gmail.com>
2023-01-21 14:10:54 +0100

    Reflect other rigidbodies with the reflector

--- Assets/Scripts/Runtime/Player/ReflectorTriggerBehaviour.cs
+++ Assets/Scripts/Runtime/Player/ReflectorTriggerBehaviour.cs
@@ -1,23 +1,38 @@
+using System;
 using UnityEngine;
 
 namespace Com.ThirdNerve.Backfire.Runtime.Player
 {
     public class ReflectorTriggerBehaviour : MonoBehaviour
     {
-        //Upon collision with another GameObject, this GameObject will reverse direction
         private Rigidbody2D _player;
-    
-        private void OnTriggerEnter2D(Collider2D other)
+        private Collider2D _reflectorCollider;
+
+        private void OnEnable()
         {
-            var angleOfIncidence = GetAngleOfIncidence(other.transform);
-        
-        
-        
+            _player = GetComponentInParent<Rigidbody2D>();
+            _reflectorCollider = GetComponent<Collider2D>();
         }
 
-        private float GetAngleOfIncidence(Transform otherTransform)
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            throw new System.NotImplementedException();
+            var otherRigidbody = other.GetComponent<Rigidbody2D>();
+            if (otherRigidbody is null)
+            {
+                return;
+            }
+            
+            var otherVelocity = otherRigidbody.velocity;
+            var playerVelocity = _player.velocity;
+
+            var combinedVelocity = playerVelocity + otherVelocity;
+
+            var reflectorCenter = _reflectorCollider.bounds.center;
+            var reflectorNormal = ((Vector2) reflectorCenter - _player.position).normalized;
+            
+            var reflectedVelocity = Vector2.Reflect(otherVelocity, reflectorNormal);
+
+            otherRigidbody.velocity = reflectedVelocity;
         }
     }
 }
