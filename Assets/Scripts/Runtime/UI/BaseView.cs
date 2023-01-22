570a8844384ce74a8a52feafda3c168a0ccaaf7c
Morrison Cole <admin@morrisoncole.co.uk>
2023-01-22 14:45:45 +0900

    fix: runtime build

--- Assets/Scripts/Runtime/UI/BaseView.cs
+++ Assets/Scripts/Runtime/UI/BaseView.cs
@@ -1,13 +1,12 @@
 ﻿using System.IO;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Com.ThirdNerve.Backfire.Runtime.UI
 {
     public class BaseView<T> : VisualElement
     {
-        private const string UxmlBasePath = "UI";
-        
         protected BaseView()
         {
             var container = GetVisualTree(typeof(T).Name);
@@ -16,8 +15,8 @@ namespace Com.ThirdNerve.Backfire.Runtime.UI
 
         private static VisualTreeAsset GetVisualTree(string name)
         {
-            var templateContainerPath = $"Assets/{UxmlBasePath}/{name}/{name}.uxml";
-            var visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(templateContainerPath);
+            var templateContainerPath = $"{name}/{name}";
+            var visualTreeAsset = Resources.Load<VisualTreeAsset>(templateContainerPath);
             if (visualTreeAsset is null)
             {
                 throw new FileNotFoundException($"Template {name} not found", templateContainerPath);
