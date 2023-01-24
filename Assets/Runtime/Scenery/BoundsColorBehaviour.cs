e1e6137a618288265eb4580705177f8f64317305
Morrison Cole <admin@morrisoncole.co.uk>
2023-01-24 21:07:07 +0900

    feat: use URP, add lighting

--- Assets/Runtime/Scenery/BoundsColorBehaviour.cs
+++ Assets/Runtime/Scenery/BoundsColorBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using Random = UnityEngine.Random;
 
 namespace Com.ThirdNerve.Backfire.Runtime.Scenery
@@ -11,10 +12,12 @@ namespace Com.ThirdNerve.Backfire.Runtime.Scenery
         private float _timeSinceLastChange;
         private Color _currentColor;
         private Color _nextColor;
+        private Light2D[]? _light2Ds;
 
         private void Awake()
         {
             _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+            _light2Ds = GetComponentsInChildren<Light2D>();
             _currentColor = Random.ColorHSV();
             _nextColor = Random.ColorHSV();
         }
@@ -30,9 +33,14 @@ namespace Com.ThirdNerve.Backfire.Runtime.Scenery
                 _timeSinceLastChange = 0f;
             }
 
+            var color = Color.Lerp(_currentColor, _nextColor, _timeSinceLastChange / changeColourTime);
             foreach (var spriteRenderer in _spriteRenderers)
             {
-                spriteRenderer.color = Color.Lerp(_currentColor, _nextColor, _timeSinceLastChange / changeColourTime);
+                spriteRenderer.color = color;
+            }
+            foreach (var light2D in _light2Ds)
+            {
+                light2D.color = color;
             }
         }
     }
